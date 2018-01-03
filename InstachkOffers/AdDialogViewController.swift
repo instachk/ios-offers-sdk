b2633d12ca9da659c199aab47e20c78f690443f8
Naresh <32950370+NareshIn@users.noreply.github.com>
2018-01-03 13:31:55 +0530

    Advertisement changes

---
+++ InstachkOffers/AdDialogViewController.swift
@@ -0,0 +1,56 @@
+//
+//  AdDialogViewController.swift
+//  offers
+//
+//  Created by Ravi Tamada on 18/07/17.
+//  Copyright © 2017 Ravi Tamada. All rights reserved.
+//
+
+import UIKit
+
+protocol AdDialogDelegate {
+    func onActivateCouponConfirmed();
+    func onAdDialogDismissed();
+}
+
+public class AdDialogViewController: UIViewController, UIGestureRecognizerDelegate {
+    
+    @IBOutlet weak var viewBackground: UIView!
+    @IBOutlet weak var adImage: UIImageView!
+    public var imageUrl: String = ""
+    var delegate: AdDialogDelegate!
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        tap.delegate = self
+        viewBackground.addGestureRecognizer(tap)
+    }
+    
+    public override func viewDidAppear(_ animated: Bool) {
+        self.view.superview?.backgroundColor = UIColor.clear
+        self.view.superview?.isOpaque = false
+        self.modalPresentationStyle = .overCurrentContext
+        self.displayAdImage()
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer? = nil) {
+        dismiss(animated: true, completion: nil)
+        self.delegate?.onAdDialogDismissed()
+    }
+
+    @IBAction func dismissClicked(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+        self.delegate?.onAdDialogDismissed()
+    }
+    
+    @IBAction func onConfirmClicked(_ sender: UIButton) {
+            dismiss(animated: true, completion: nil)
+            self.delegate?.onActivateCouponConfirmed()
+    }
+    
+    func displayAdImage() {
+        self.adImage.downloadedFrom(link: imageUrl)
+    }
+}
